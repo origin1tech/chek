d50e7aa5d700009d0dd1bbabfa2716d3476bf708
origin1tech <origin1tech@gmail.com>
2017-07-12 09:05:31 -0700

    add missing file

---
+++ dist/chek.js
@@ -0,0 +1,15 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./modules/array"));
+__export(require("./modules/constant"));
+__export(require("./modules/from"));
+__export(require("./modules/function"));
+__export(require("./modules/is"));
+__export(require("./modules/object"));
+__export(require("./modules/string"));
+__export(require("./modules/to"));
+__export(require("./modules/type"));
+//# sourceMappingURL=chek.js.map
